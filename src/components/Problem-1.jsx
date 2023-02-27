cb2b44bf5a2f9d627cffb701c55b48c5e5e087ef
Md Hasan <mdhasan8064@gmail.com>
2023-02-27 15:34:52 +0600

    install sass & solve Problem 1

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -4,9 +4,55 @@ const Problem1 = () => {
 
     const [show, setShow] = useState('all');
 
+    // All data Store in it
+    const [allData, setAllData] = useState([]);
+    const [showUiData, setShowUiData] = useState([]);
+    
+
+    // make a sort array for show in all tab
+    const sortByStatus = (array) => {
+        const activeData = array.filter(statusData => statusData.status.toLowerCase() == 'active');
+        const complated = array.filter(statusData => statusData.status.toLowerCase() == 'completed');
+        const othersData = array.filter(statusData => statusData.status.toLowerCase() !== 'active' && statusData.status.toLowerCase() !== "completed");
+
+        //Create new array sorting status
+        const finalArray = [...activeData, ...complated, ...othersData];
+        return finalArray;
+    }
+
+    // Handle Form data 
+    const handleForm = (event) =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const status = form.status.value;
+        const newStatusObj = {name, status};
+
+        // show data on all tab 
+        setAllData([...allData, newStatusObj]);
+        const sortData = sortByStatus([...allData, newStatusObj]);
+        setShowUiData([...sortData])
+        // console.log(newStatusObj)
+        form.reset()
+    }
+
+// console.log(showUiData)
+
     const handleClick = (val) =>{
         setShow(val);
+
+        // Show only all data
+        if(val == 'all'){
+            const sortData = sortByStatus([...allData]);
+            setShowUiData([...sortData]);
+            return 
+        }
+
+        // it will Active and Completed data depend of the value/tab with filter
+        const filterForShowUi = allData.filter(data => data.status.toLowerCase() == val );
+        setShowUiData([...filterForShowUi])
     }
+
 
     return (
 
@@ -14,12 +60,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form onSubmit={handleForm} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" className="form-control" name="name" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" className="form-control" name="status" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -47,7 +93,13 @@ const Problem1 = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        
+                            {
+                                showUiData.length > 0 && showUiData.map((data, index) =>
+                                <tr key={index}>
+                                    <td>{data.name}</td>
+                                    <td>{data.status}</td>
+                                </tr>
+                            )}
                         </tbody>
                     </table>
                 </div>
